1dd46cc108c6879b42e5273859f3dc9cc556536c
rich <rlucas11@yahoo.com>
2023-02-09 19:52:46 -0500

    Made the app reactive

--- app.R
+++ app.R
@@ -16,21 +16,17 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            h4("See below for instructions"),
-            fluidRow(
-                column(6, numericInput("w",
-                    "Number of Waves:",
-                    min = 2,
-                    max = 50,
-                    value = 10,
-                    width = "60%"
-                )),
-                column(4, actionButton(
-                    "update",
-                    "Update Stability"
-                ))
-            ),
-            h4("Enter Data (Optional)"),
+            p("This app generates data based on the parameter values you specify. It then shows what the patterns of stabilities would look like for increasingly long intervals. You can also upload a correlation matrix for your own two-variable data to compare."),
+            numericInput("w",
+                         "Number of Waves:",
+                         min = 2,
+                         max = 50,
+                         value = 10,
+                         width = "30%"
+                         ),
+            helpText("This option is ignored if you provide your own data. The number of waves will be set to the number of waves in the data that you provide"),
+            br(),
+            h4("Enter Your Own Data (Optional)"),
             fileInput(
                 "file1",
                 "Choose CSV File",
@@ -41,8 +37,11 @@ ui <- fluidPage(
                 ),
                 width = "50%"
             ),
+            helpText("You can optionally upload a matrix of correlations (with the same number of waves as you specify in the app) and the app will plot these stabilities, too. You should upload a symmetric correlation matrix as a csv file, and the variables should be ordered by wave (e.g., X1, Y1, X2, Y2,...)"),
             p(),
-            h4("Variances (Using STARTS Terminology)"),
+            h2("Specify The Data Generating Model"),
+            helpText("The model to the right shows the possible components you can include. If you include stable trait variance, state variance, and autoregressive variance, the data-generating model will be the STARTS model. You can specify variance components to be zero to fit reduced models (like the RI-CLPM or CLPM)."),
+            h4("Variances"),
             fluidRow(
                 column(4, numericInput("st_x",
                                        "X Stable Trait",
@@ -59,7 +58,6 @@ ui <- fluidPage(
                                        min = 0,
                                        step = .05,
                                        value = 1))),
-            h4("Y Variable"),
             fluidRow(
                 column(4, numericInput("st_y",
                                        "Y Stable Trait",
@@ -120,10 +118,8 @@ ui <- fluidPage(
                         value = .5))
             ),
             h4("How To Use This App"),
-            p("This app generates data based on the parameter values you specify. It then shows what the patterns of stabilities would look like for increasingly long intervals. You can optionally upload a matrix of correlations (with the same number of waves as you specify in the app) and the app will plot these stabilities, too. You should upload a symmetric correlation matrix as a csv file, and the variables should be ordered by wave (e.g., X1, Y1, X2, Y2,...)"),
-            p("The model to the right shows the possible components you can include. If you include stable trait variance, state variance, and autoregressive variance, the data-generating model will be the STARTS model. You can specify variance components to be zero to fit reduced models (like the RI-CLPM or CLPM)."),
             p("Also note that you can specify values for which the data are impossible to generate (e.g., high stability plus strong cross-lagged paths). I don't have good error checking for this yet, so if the page freezes, just reload and try diffrent values."),
-            "Source code is available here:", tags$a(href="https://github.com/rlucas11/compare-cor", target="_blank", "https://github.com/rlucas11/compare-cor")
+            p("Source code is available here:", tags$a(href = "https://github.com/rlucas11/compare-cor", target = "_blank", "https://github.com/rlucas11/compare-cor"))
         ),
 
         # Show data generating model and results
@@ -329,45 +325,45 @@ server <- function(input, output) {
   )})
     
     
-  
-    observeEvent(input$update, {
+    output$corPlot <- renderPlot({
         ## Check for and load user data
         inFile <- input$file1
         if (!is.null(inFile)) {
             userInput <- TRUE
             userCorMat <- as.matrix(read.csv(inFile$datapath))
             userCors <- summarizeR(userCorMat, 2)
+            nwaves <- ncol(userCorMat) / 2
         } else {
             userInput <- FALSE
+            nwaves <- input$w
         }
         data <- gen_starts(
-            n=10000,
-            nwaves=input$w,   # Number of waves
-            ri_x=input$st_x,     # Random intercept variance for X
-            ri_y=input$st_y,     # Random intercept variance for Y
-            cor_i=input$st_cor,   # Correlation between intercepts (as correlation)
-            x=input$ar_x,        # AR variance for X
-            y=input$ar_y,        # AR variance for Y
-            stab_x=input$stability_x,  # Stability of X
-            stab_y=input$stability_y,  # Stability of Y
-            yx=input$YonX,      # Cross lag (Y regressed on X)
-            xy=input$XonY,      # Cross lag (X regressed on Y)
-            cor_xy=input$ar_cor,  # Correlation between X and Y (as correlation)
-            xr=input$state_x,       # Measurement error for X
-            yr=input$state_y        # Measurement error for Y
+            n = 10000,
+            nwaves = nwaves, # Number of waves
+            ri_x = input$st_x, # Random intercept variance for X
+            ri_y = input$st_y, # Random intercept variance for Y
+            cor_i = input$st_cor, # Correlation between intercepts (as correlation)
+            x = input$ar_x, # AR variance for X
+            y = input$ar_y, # AR variance for Y
+            stab_x = input$stability_x, # Stability of X
+            stab_y = input$stability_y, # Stability of Y
+            yx = input$YonX, # Cross lag (Y regressed on X)
+            xy = input$XonY, # Cross lag (X regressed on Y)
+            cor_xy = input$ar_cor, # Correlation between X and Y (as correlation)
+            xr = input$state_x, # Measurement error for X
+            yr = input$state_y # Measurement error for Y
         )
         ## Reorder generated data for summarizeR
-        data <- data[, paste0(c("x", "y"), rep(1:input$w, each = 2))]
+        data <- data[, paste0(c("x", "y"), rep(1:nwaves, each = 2))]
         cors <- summarizeR(cor(data), 2)
         if (!is.null(inFile)) {
             cors <- cbind(cors, userCors)
         }
-        output$corPlot <- renderPlot({
-            plotCors(cors, user=userInput)
+            plotCors(cors, user = userInput)
         })
-    }
-    )
 }
+
+
 
     
 
